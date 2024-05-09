55b813e5dee327ebfe6d7f98b9b6e9895033e047
Adam KHNAGUI <144346140+SpicyFire21@users.noreply.github.com>
2024-05-09 13:52:31 +0200

    Add files via upload

--- static/src/JS/script.js
+++ static/src/JS/script.js
@@ -1,73 +1,101 @@
-// function fetchData() {
-//     const tableElement = document.createElement("table");
-//     tableElement.classList.add("table-agent");
-//     document.body.appendChild(tableElement);
-//
-//     fetch("https://valorant-api.com/v1/agents")
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((data) => {
-//             const agents = data.data;
-//
-//             console.log(agents);
-//
-//
-//             const headerRow = document.createElement("tr");
-//             //const uuidHeader = document.createElement("th");
-//             const nameHeader = document.createElement("th");
-//
-//             //uuidHeader.textContent = "UUID"
-//             nameHeader.textContent = "NOM";
-//
-//             const descriptionHeader = document.createElement("th");
-//             descriptionHeader.textContent = "ROLE";
-//
-//             const iconHeader = document.createElement("th");
-//             iconHeader.textContent = "ICONE";
-//
-//             //headerRow.appendChild(uuidHeader)
-//             headerRow.appendChild(nameHeader);
-//             headerRow.appendChild(descriptionHeader);
-//             headerRow.appendChild(iconHeader)
-//             tableElement.appendChild(headerRow);
-//
-//
-//             for (const agent of agents) {
-//                 if (agent.isPlayableCharacter === true) {
-//                     const row = document.createElement("tr");
-//
-//                     //const uuidCell = document.createElement("td")
-//                     //uuidCell.textContent = agent.uuid;
-//                     //row.appendChild(uuidCell);
-//
-//                     // Cellule pour le nom
-//                     const nameCell = document.createElement("td");
-//                     nameCell.textContent = agent.displayName;
-//                     row.appendChild(nameCell);
-//
-//                     // Cellule pour la description
-//                     const roleCell = document.createElement("td");
-//                     roleCell.textContent = agent.role.displayName;
-//                     row.appendChild(roleCell);
-//
-//
-//                     // Cellule pour la description
-//                     const iconCell = document.createElement("td");
-//                     const imgIcone = document.createElement("img");
-//                     iconCell.classList.add("td-icones");
-//                     imgIcone.classList.add("agent-icones");
-//                     imgIcone.setAttribute("src", agent.displayIconSmall)
-//                     iconCell.appendChild(imgIcone)
-//                     row.appendChild(iconCell);
-//
-//                     tableElement.appendChild(row);
-//                 }
-//             }
-//         })
-//         .catch((error) => {
-//             console.error('Une erreur s\'est produite : ', error);
-//         });
-// }
+// ######################################
+// carousel
+// ######################################
+
+const slide = ["/static/assets/img/carrousel-1.png",
+    "/static/assets/img/carrousel-2.png",
+    "/static/assets/img/carrousel-3.png",
+    "/static/assets/img/carrousel-4.png",
+    "/static/assets/img/carrousel-5.png"];
+let index = 0;
+let index_old = index - 1;
+let index_new = index + 1;
+
+const timeStamp = 5000; //ms
+let autoSlide
+if (timeStamp <= 800){
+    ntimeStamp = timeStamp + 200;
+} else {
+    ntimeStamp = timeStamp
+}
+let transitionDelay =timeStamp/4;
+
+if (transitionDelay > 800) {
+    transitionDelay = 800;
+}
+
+
+
+
+function ChangeSlide(sens) {
+    clearTimeout(autoSlide);
+
+
+    const previousButton = document.querySelector('#previous');
+    const nextButton = document.querySelector('#next');
+    previousButton.style.display = "none";
+    nextButton.style.display = "none";
+
+
+
+    index = index + sens;
+    index_old = index_old + sens;
+    index_new = index_new + sens;
+    if (index < 0) {
+        index = slide.length - 1;
+    }
+    if (index > slide.length - 1) {
+        index = 0;
+    }
+    if (index_old < 0) {
+        index_old = slide.length - 1;
+    }
+    if (index_old > slide.length - 1) {
+        index_old = 0;
+    }
+    if (index_new < 0) {
+        index_new = slide.length - 1;
+    }
+    if (index_new > slide.length - 1) {
+        index_new = 0;
+    }
+    const WidthCarrousel = document.querySelector('.slider-1').offsetWidth; // la bordure est compté dans la longueur donc je retire les pixels en trop
+    const divslide = document.getElementById("container");
+
+    if (sens > 0) {
+        divslide.style.transition = "transform "+ transitionDelay +"ms ease";
+        divslide.style.transform = "translateX(-" + WidthCarrousel + "px)";
+        setTimeout(() => {
+            divslide.style.transition = "";
+            divslide.style.transform = "";
+            document.getElementById("slide-old").src = slide[index_old];
+            document.getElementById("slide").src = slide[index];
+            document.getElementById("slide-new").src = slide[index_new];
+            previousButton.style.display = "";
+            nextButton.style.display = "";
+
+        }, transitionDelay + 50);
+    } else if (sens < 0) {
+        const divslide = document.getElementById("container");
+        divslide.style.transition = "transform "+ transitionDelay +"ms ease";
+        divslide.style.transform = "translateX(" + WidthCarrousel + "px)";
+        setTimeout(() => {
+            divslide.style.transition = "";
+            divslide.style.transform = "";
+            document.getElementById("slide-old").src = slide[index_old];
+            document.getElementById("slide").src = slide[index];
+            document.getElementById("slide-new").src = slide[index_new];
+            previousButton.style.display = "";
+            nextButton.style.display = "";
+
+        }, transitionDelay + 50);
+
+    }
+
+    autoSlide =setTimeout(() => ChangeSlide(1), ntimeStamp);
+}
+
+setTimeout(() => ChangeSlide(1), ntimeStamp);
+
 
 
